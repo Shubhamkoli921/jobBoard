0314421f3655d4d541fa971c36c621950af74ba0
Paras bora <shubhamkk922@gmail.com>
2024-03-03 10:18:47 +0530

    port2

--- server/server.js
+++ server/server.js
@@ -28,5 +28,5 @@ app.use('/job',application)
 
 const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
