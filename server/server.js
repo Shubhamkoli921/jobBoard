b01259703893e5f7f6b24a2c5e513fd3e947c4fc
Paras bora <shubhamkk922@gmail.com>
2024-03-04 22:31:30 +0530

    server

--- server/server.js
+++ server/server.js
@@ -30,5 +30,5 @@ app.use('/job', authenticateUser, application);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on this http://localhost:${PORT} port`);
 });
